ee2aba488f836072bb97854ef859cc91c03e7682
Andre Coelho <andre.junior@adp.com>
2022-08-10 15:05:26 -0300

    refact menu-mobile class

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
+import MenuMobile from './modules/menu-mobile.js';
 import initOperation from './modules/operation.js';
 import fetchAnimals from './modules/animals-api.js';
 import fetchBitcoin from './modules/bitcoinapi.js';
@@ -31,7 +31,9 @@ animateScroll.init();
 const dropDownMenu = new DropdownMenu('[data-dropdown');
 dropDownMenu.init();
 
-initMenuMobile();
+const menuMobile = new MenuMobile('[data-menu="button"', '[data-menu="list"');
+menuMobile.init();
+
 initOperation();
 fetchAnimals('../../animalsapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btcPrice');
